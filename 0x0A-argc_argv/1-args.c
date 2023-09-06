98c90b6524e08d8169ba819c4c83bf0820de5728
TheManuel01 <eawe439@gmail.com>
2023-09-06 21:41:14 +0100

    my arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,20 +3,22 @@
 
 /**
  * main - a program that prints the number of arguments passed into it
+ *
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
+ *
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char __attribute__((unused)) *argv[])
 {
-		int i = 0, m;
+	int i = 0, m;
 
-		while (i < argc)
-		{
-			m = i;	
-			i++;
-		}
-		printf("%d\n", m);
-		return (0);
+	while (i < argc)
+	{
+		m = i;
+		i++;
+	}
+	printf("%d\n", m);
+	return (0);
 }
